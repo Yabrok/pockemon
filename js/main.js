5255c699ecc336f18116e92ec7b0246a2d306601
Yabrok <nosor321@mail.ru>
2022-12-21 01:10:54 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -2,31 +2,28 @@ var elForm = document.querySelector('.js-form');
 var elList = document.querySelector('.js-list');
 var elSelect = document.querySelector('.js-select');
 
-var newArr = [];
-elSelect.addEventListener('change', function(){
-  elList.innerHTML = '';
-  newArr = [];
-  if (elSelect.value != 'All'){
-    pokemons.forEach((el) => {
-      if (el.type.includes(elSelect.value)){
-        newArr.push(el);
-        render(newArr);
-
-      }
-    })
-  }else {
-    render(pokemons)
-  }
-  
-
-  // render(newArr);
+let newSet = new Set();
+pokemons.forEach((item)=>{
+  item.type.forEach((type)=>{
+    newSet.add(type);
+  });
 });
 
-  function render(arr){
-    for (pokemon of arr){
-      var elItem = document.createElement('li');
-      var pokPic = document.createElement('img')
-      pokPic.setAttribute('src', pokemon.img)
+newSet.forEach((type)=>{
+  let elOption = document.createElement('option');
+  elOption.value = type;
+  elOption.textContent = type;
+  elSelect.appendChild(elOption)
+})
+
+
+
+function render(arr, node){
+  node.innerHTML = '';
+  for (pokemon of arr){
+    var elItem = document.createElement('li');
+    var pokPic = document.createElement('img')
+    pokPic.setAttribute('src', pokemon.img)
       elItem.appendChild(pokPic);
   
       var pokName = document.createElement('h3');
@@ -40,53 +37,35 @@ elSelect.addEventListener('change', function(){
       var pokType = document.createElement('p');
       pokType.textContent = pokemon.type
       elItem.appendChild(pokType);
-  
+      
       var pokCandy = document.createElement('p');
       pokCandy.textContent = pokemon.candy
       elItem.appendChild(pokCandy);
-  
+      
       elItem.classList.add("d-flex", "flex-column", "align-items-center", "border", "border-danger", "bg-danger", "text-white")
-  
+      
       elItem.style.width = '300px'
       elItem.style.marginBottom = '30px'
       elItem.style.borderRadius = '10px'
       
-  
-      elList.appendChild(elItem);
+      
+      node.appendChild(elItem);
     }
   }
-
-  render(pokemons);
-
-
-  // var newArr = [];
-  // elSelect.addEventListener('change', function(){
-  //   newArr = [];
-  //   pokemons.forEach((pok) => {
-  //     if (pok.type.includes(elSelect.value)){
-  //       newArr.push(pok)
-  //     }
-  //   });
-  //   console.log(newArr);
-    
-  // })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  render(pokemons, elList);
+  
+  
+  let newArr = [];
+  elForm.addEventListener('change', ()=>{
+    newArr = [];
+    if (elSelect.value != 'All'){
+      pokemons.forEach((el)=>{
+        if (el.type.includes(elSelect.value)){
+          newArr.push(el);
+          render(newArr, elList);
+        };
+      });
+    }else {
+      render(pokemons, elList);
+    }
+  });
